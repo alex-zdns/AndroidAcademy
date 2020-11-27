891e9d3bd02e4170008014be839a25a20c17e44c
Aleksandr Zdanovich <alex.zdns@gmail.com>
2020-11-27 18:47:47 +0700

    added to handle device rotation

--- app/src/main/java/ru/alexzdns/fundamentals/homework/MainActivity.kt
+++ app/src/main/java/ru/alexzdns/fundamentals/homework/MainActivity.kt
@@ -3,19 +3,23 @@ package ru.alexzdns.fundamentals.homework
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), MovieListFragment.MovieListClickListener,
+class MainActivity : AppCompatActivity(),
+    MovieListFragment.MovieListClickListener,
     MovieDetailsFragment.MovieDetailsClickListener {
-    private val movieListFragment =
-        MovieListFragment().apply { setListener(this@MainActivity) }
-
+    private var movieListFragment : MovieListFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-            .add(R.id.ma_main_container, movieListFragment)
-            .commit()
+        if (savedInstanceState == null) {
+            movieListFragment = MovieListFragment().apply { setListener(this@MainActivity)}
+            supportFragmentManager.beginTransaction()
+                .add(R.id.ma_main_container, movieListFragment!!, MOVIE_LIST_FRAGMENT)
+                .commit()
+        } else {
+            movieListFragment = supportFragmentManager.findFragmentByTag(MOVIE_LIST_FRAGMENT) as? MovieListFragment
+        }
     }
 
     override fun openMovieDetailsFragment() {
@@ -33,5 +37,9 @@ class MainActivity : AppCompatActivity(), MovieListFragment.MovieListClickListen
         supportFragmentManager.beginTransaction()
             .remove(lastFragment)
             .commit()
+    }
+
+    companion object {
+        const val MOVIE_LIST_FRAGMENT = "movieListFragment"
     }
 }
