0313ee03c4d16eaf736d52e6ccb104b8f09319f7
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-01-13 01:05:56 +0500

    make retrofit create() more shortly

--- app/src/main/java/ru/alexzdns/fundamentals/homework/ui/movieDetails/MovieDetailsViewModelFactory.kt
+++ app/src/main/java/ru/alexzdns/fundamentals/homework/ui/movieDetails/MovieDetailsViewModelFactory.kt
@@ -2,13 +2,13 @@ package ru.alexzdns.fundamentals.homework.ui.movieDetails
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import ru.alexzdns.fundamentals.homework.network.MovieApi
+import retrofit2.create
 import ru.alexzdns.fundamentals.homework.network.NetworkModule
 
 class MovieDetailsViewModelFactory(): ViewModelProvider.Factory {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when (modelClass) {
-        MovieDetailsViewModel::class.java -> MovieDetailsViewModel(NetworkModule.retrofit.create(MovieApi::class.java))
+        MovieDetailsViewModel::class.java -> MovieDetailsViewModel(NetworkModule.retrofit.create())
         else -> throw IllegalArgumentException("$modelClass is not registered ViewModel")
     } as T
 }
--- app/src/main/java/ru/alexzdns/fundamentals/homework/ui/moviesList/MoviesListViewModelFactory.kt
+++ app/src/main/java/ru/alexzdns/fundamentals/homework/ui/moviesList/MoviesListViewModelFactory.kt
@@ -2,14 +2,14 @@ package ru.alexzdns.fundamentals.homework.ui.moviesList
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import ru.alexzdns.fundamentals.homework.network.MovieApi
+import retrofit2.create
 import ru.alexzdns.fundamentals.homework.network.NetworkModule
 
 class MoviesListViewModelFactory : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel?> create(modelClass: Class<T>): T = when (modelClass) {
-        MoviesListViewModel::class.java -> MoviesListViewModel(NetworkModule.retrofit.create(MovieApi::class.java))
+        MoviesListViewModel::class.java -> MoviesListViewModel(NetworkModule.retrofit.create())
         else -> throw IllegalArgumentException("$modelClass is not registered ViewModel")
     } as T
 }
