8f39387b7d325e82ee28ff39f3fdfbb7146ff1f4
Aleksandr Zdanovich <alex.zdns@gmail.com>
2021-01-23 17:57:43 +0500

    add glide caching

--- app/src/main/java/ru/alexzdns/fundamentals/homework/ui/movieDetails/ActorsAdapter.kt
+++ app/src/main/java/ru/alexzdns/fundamentals/homework/ui/movieDetails/ActorsAdapter.kt
@@ -7,6 +7,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -40,6 +41,7 @@ class ActorsAdapter(
         fun bind(actor: Actor) {
             Glide.with(itemView.context)
                 .load(actor.picture)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .apply(imageOption)
                 .into(avatar)
 
--- app/src/main/java/ru/alexzdns/fundamentals/homework/ui/moviesList/MoviesAdapter.kt
+++ app/src/main/java/ru/alexzdns/fundamentals/homework/ui/moviesList/MoviesAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import ru.alexzdns.fundamentals.homework.R
 import ru.alexzdns.fundamentals.homework.data.models.Movie
@@ -50,6 +51,7 @@ class MoviesAdapter(
         fun bind(movie: Movie) {
             Glide.with(itemView.context)
                 .load(movie.poster)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .apply(imageOption)
                 .into(banner)
 
